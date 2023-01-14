4395502aa8ea2f1b1ff17c82168de7d5dd38846a
Christine <yhc59go@gmail.com>
2023-01-15 03:02:45 +0800

    [DEV] Change the naming rule of class variable
    Change the naming rule of class variable.

--- model/communicateWithS3.py
+++ model/communicateWithS3.py
@@ -16,16 +16,16 @@ class communicateWithS3():
     def __init__(self):
         # 建立 S3 资源
         self.s3 = boto3.resource('s3',
-                    aws_access_key_id=self.AWS_ACCESS_KEY_ID,
-                    aws_secret_access_key=self.AWS_SECRET_ACCESS_KEY)
+                    aws_access_key_id=communicateWithS3.AWS_ACCESS_KEY_ID,
+                    aws_secret_access_key=communicateWithS3.AWS_SECRET_ACCESS_KEY)
 
     def uploadImage(self,imageName,imageBody):
-        self.s3.Bucket(self.S3_BUCKET_NAME).upload_fileobj(imageBody, imageName,ExtraArgs={'ContentType': 'image/jpeg','ACL':'public-read'})
+        self.s3.Bucket(communicateWithS3.S3_BUCKET_NAME).upload_fileobj(imageBody, imageName,ExtraArgs={'ContentType': 'image/jpeg','ACL':'public-read'})
     
     def showAllObjectInS3(self):
         # 回傳在S3的所有物件名字
         AllObjectInS3=[]  
-        for obj in boto3.resource('s3').Bucket(self.S3_BUCKET_NAME).objects.all():
+        for obj in boto3.resource('s3').Bucket(communicateWithS3.S3_BUCKET_NAME).objects.all():
             AllObjectInS3.append(obj.key)
         return AllObjectInS3
         
